7dcdeca2656a43a99eb03a1d1e8ddee8167ddc10
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-08-30 23:31:53 +0300

    School entity için repository, interface, request, response yapıları eklendi.

---
+++ src/Core/ProductExample.Application/Features/School/Commands/CreateSchool/CreateSchoolCommandRequest.cs
@@ -0,0 +1,18 @@
+﻿using Lms.Application.Features.Base;
+using System;
+
+namespace Lms.Application.Features.School.Commands.CreateSchool
+{
+    public class CreateSchoolCommandRequest : BaseRequest
+    {
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? Detail { get; set; }
+        public string? Note { get; set; }
+    }
+}

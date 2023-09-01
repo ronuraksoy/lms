2603c3eecbb020f20c89114ce4d0d81540d0099b
Furkan Coşkun <furkancoskunn96@gmail.com>
2023-09-02 01:57:33 +0300

    Classrooms için repository, interface, commands, queries, validator yapıları eklendi.

---
+++ src/Core/ProductExample.Application/Features/Classrooms/Queries/GetClassroomsPagedList/GetClassroomsPagedListQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Lms.Application.Interfaces.Repositories;
+using Lms.Comman.Domain.Entities;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lms.Application.Features.Classrooms.Queries.GetClassroomsPagedList
+{
+    public class GetClassroomsPagedListQueryHandler : IRequestHandler<GetClassroomsPagedListQueryRequest, IPagedDataResult<GetClassroomsPagedListQueryResponse>>
+    {
+        readonly IClassroomsRepository _classroomsRepository;
+        IMapper _mapper;
+        public GetClassroomsPagedListQueryHandler(IClassroomsRepository classroomsRepository, IMapper mapper)
+        {
+            _classroomsRepository = classroomsRepository;
+            _mapper = mapper;
+        }
+        public async Task<IPagedDataResult<GetClassroomsPagedListQueryResponse>> Handle(GetClassroomsPagedListQueryRequest request, CancellationToken cancellationToken)
+        {
+            var data = await _classroomsRepository.GetPagedList(request);
+            var result = _mapper.Map<IEnumerable<GetClassroomsPagedListQueryResponse>>(data.Data);
+            return new PagedDataResult<GetClassroomsPagedListQueryResponse>(data.RecordsTotal, data.RecordsFiltered, result, true, "Başarılı işlem...");
+        }
+    }
+}
---
+++ src/Infrastructure/ProductExample.Persistence/Repositories/ClassroomsRepository.cs
@@ -0,0 +1,25 @@
+﻿using Lms.Application.Features.Classrooms.Queries.GetClassroomsPagedList;
+using Lms.Application.Interfaces.Repositories;
+using Lms.Comman.Domain.Entities;
+using Lms.Comman.Persistence.Repositories;
+using Lms.Domain.Entities;
+using Lms.Persistence.Context;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lms.Persistence.Repositories
+{
+    public class ClassroomsRepository : AAuditableRepository<Classrooms, ApplicationDbContext>, IClassroomsRepository
+    {
+        public ClassroomsRepository(ApplicationDbContext dbContext, ILogger<Classrooms> logger) : base(dbContext, logger)
+        {
+        }
+
+        public Task<IPagedData<Classrooms>> GetPagedList(GetClassroomsPagedListQueryRequest model, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

591df5935e945dcebbfe7fbd267494831b7a3aa2
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-09-01 20:35:22 +0300

    Semester için repository, interface, command, request yapıları oluşturuldu.

---
+++ src/Core/ProductExample.Application/Features/Semester/Queries/GetSemesterList/GetSemesterListQueryResponse.cs
@@ -0,0 +1,9 @@
+﻿using Lms.Application.Features.Semester.Queries.GetSemesterById;
+
+namespace Lms.Application.Features.Semester.Queries.GetSemesterList
+{
+    public class GetSemesterListQueryResponse : GetSemesterByIdQueryResponse //Tabloda görünmesini istediğimiz bilgiler
+    {
+
+    }
+}

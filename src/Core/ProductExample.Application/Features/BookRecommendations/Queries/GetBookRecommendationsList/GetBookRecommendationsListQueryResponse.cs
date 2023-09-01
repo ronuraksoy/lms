4db9a19098659c5fd4b77abb46b2258fa91fa005
Furkan Coşkun <furkancoskunn96@gmail.com>
2023-09-02 01:19:00 +0300

    BookRecommendations için repository, interface, commands, queries, validator yapıları eklendi.

---
+++ src/Core/ProductExample.Application/Features/BookRecommendations/Queries/GetBookRecommendationsList/GetBookRecommendationsListQueryResponse.cs
@@ -0,0 +1,9 @@
+﻿using Lms.Application.Features.BookRecommendations.Queries.GetBookRecommendationsById;
+
+namespace Lms.Application.Features.BookRecommendations.Queries.GetBookRecommendationsList
+{
+    public class GetBookRecommendationsListQueryResponse : GetBookRecommendationsByIdQueryResponse //Tabloda görünmesini istediğimiz bilgiler
+    {
+
+    }
+}

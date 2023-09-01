4777d9232a43503cbc3ba7c5c6ab752087530007
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-09-01 20:59:01 +0300

    User için repository, interface, command, request yapıları oluşturuldu.

---
+++ src/Core/ProductExample.Application/Features/User/Queries/GetUserById/GetUserByIdQueryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Lms.Application.Features.User.Queries.GetUserById
+{
+    public class GetUserByIdQueryResponse
+    {
+        public string Name { get; set; }
+        public int Hour { get; set; }
+    }
+}

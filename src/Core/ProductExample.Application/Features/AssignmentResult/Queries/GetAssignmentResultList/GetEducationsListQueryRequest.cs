0e486d8aaa437f466bbc6db30b6b0eae5750eb6d
Furkan Coşkun <furkancoskunn96@gmail.com>
2023-09-02 00:55:03 +0300

    AssignmentResult için repository, interface, commands, queries, validator yapıları eklendi.

---
+++ src/Core/ProductExample.Application/Features/AssignmentResult/Queries/GetAssignmentResultList/GetEducationsListQueryRequest.cs
@@ -0,0 +1,10 @@
+﻿using Lms.Comman.Domain.Entities;
+using MediatR;
+using System.Collections.Generic;
+
+namespace Lms.Application.Features.AssignmentResult.Queries.GetAssignmentResultList
+{
+    public class GetAssignmentResultListQueryRequest : IRequest<IDataResult<IEnumerable<GetAssignmentResultListQueryResponse>>>
+    {
+    }
+}
---
+++ src/Core/ProductExample.Application/Features/AssignmentResult/Queries/GetAssignmentResultPagedList/GetEducationsPagedListQueryRequest.cs
@@ -0,0 +1,11 @@
+﻿using Lms.Comman.Domain.Entities;
+using Lms.Common.Application.Dtos.Datatables;
+using MediatR;
+
+namespace Lms.Application.Features.AssignmentResult.Queries.GetAssignmentResultPagedList
+{
+    public class GetAssignmentResultPagedListQueryRequest : DtParameters, IRequest<IPagedDataResult<GetAssignmentResultPagedListQueryResponse>>
+    {
+        public string? NameFilter { get; set; }
+    }
+}

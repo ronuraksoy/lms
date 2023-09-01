d84a1e1a21d16c787c4255660d623e6247cd257d
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-09-01 21:07:27 +0300

    Student için repository, interface, command, request yapıları oluşturuldu.

---
+++ src/Core/ProductExample.Application/Features/Student/Queries/GetStudentList/GetStudentListQueryResponse.cs
@@ -0,0 +1,9 @@
+﻿using Lms.Application.Features.Student.Queries.GetStudentById;
+
+namespace Lms.Application.Features.Student.Queries.GetStudentList
+{
+    public class GetStudentListQueryResponse : GetStudentByIdQueryResponse //Tabloda görünmesini istediğimiz bilgiler
+    {
+
+    }
+}
---
+++ src/Core/ProductExample.Application/Features/Student/Queries/GetStudentPagedList/GetStudentPagedListQueryResponse.cs
@@ -0,0 +1,11 @@
+﻿using Lms.Comman.Domain.Entities;
+using System;
+
+namespace Lms.Application.Features.Student.Queries.GetStudentPagedList
+{
+    public class GetStudentPagedListQueryResponse : AAuditableEntity<Guid> //Tabloda görünmesini istediğimiz bilgiler
+    {
+        public string Name { get; set; }
+        public int Hour { get; set; }
+    }
+}

92446da62eb721944e7afe47a164845f7d551539
Furkan Coşkun <furkancoskunn96@gmail.com>
2023-09-02 21:25:24 +0300

    Faculty için repositroy, interface, command, query, validator yapıları eklendi.

---
+++ src/Core/ProductExample.Application/Features/Faculty/Queries/GetFacultyById/GetFacultyByIdQueryResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Lms.Application.Features.Faculty.Queries.GetFacultyById
+{
+    public class GetFacultyByIdQueryResponse
+    {
+    }
+}
---
+++ src/Core/ProductExample.Application/Features/Faculty/Queries/GetFacultyList/GetFacultyListQueryResponse.cs
@@ -0,0 +1,9 @@
+﻿using Lms.Application.Features.Faculty.Queries.GetFacultyById;
+
+namespace Lms.Application.Features.Faculty.Queries.GetFacultyList
+{
+    public class GetFacultyListQueryResponse : GetFacultyByIdQueryResponse //Tabloda görünmesini istediğimiz bilgiler
+    {
+
+    }
+}
